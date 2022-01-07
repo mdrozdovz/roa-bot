d824c6cc9c464f8f85a74fcbfb3243734d5bbf41
mdrozdovz <somebody.put.something@gmail.com>
2022-01-07 22:15:30 +0000

    - Reworked quest completion to be general

--- roa-bot-main.js
+++ roa-bot-main.js
@@ -27,18 +27,54 @@
         questCompletion: {
             enabled: true,
             jumpForwardTimes: 0,
+            checkIntervalSeconds: 20,
         }
     };
 
-    const safeClick = async (button, postDelay) => {
-        button && button.click();
-        if (postDelay) await delay(postDelay);
+    window.roaBot = {
+        timers,
+        settings,
+        restart() {
+            this.stop();
+            start();
+        },
+        stop() {
+            log('Stopping RoA Bot');
+            for (const [key, val] of Object.entries(timers)) {
+                clearInterval(val);
+                delete timers[key];
+            }
+        },
     }
+
+    const printTimers = () => {
+        for (const [key, val] of Object.entries(timers)) {
+            log(`timers.${key}: ${val}`);
+        }
+    }
+
+    const log = msg => {
+        const now = new Date();
+        console.log(`[${now.toDateString()} ${now.toTimeString().split(' ')[0]}][RoA Bot]: ${msg}`);
+    }
+
+    const safeClick = button => button && button.click && button.click();
+
+    const getCurrentQuestType = () => {
+        const battle = $('#battleQuest');
+        const tradeskill = $('#tradeskillQuest');
+        const profession = $('#professionQuest');
+
+        // TODO re-do this shit
+        if (!/display: none/g.test(battle.attributes.style.value)) return 'battle';
+        if (!/display: none/g.test(tradeskill.attributes.style.value)) return 'tradeskill';
+        if (!/display: none/g.test(profession.attributes.style.value)) return 'profession';
+    };
 
     const setupAutoRefresh = () => {
         const replenishSelector = () => $('#replenishStamina');
 
-        console.info('Setting up auto refresh interval');
+        log('Setting up auto refresh interval');
         return setInterval(async () => {
             const item = replenishSelector();
             if (item) item.click();
@@ -46,28 +82,44 @@
     };
 
     const setupQuestCompletion = () => {
-        const postClickDelayMillis = 50;
-        const infoLinkSelector = () => $('#bq_info > a.questCenter');
-        const completeButtonSelector = () => $('#battleQuestComplete > div > input.completeQuest');
+        const infoLinkSelector = () => $('div#questInfo > div > div.center > a.questCenter');
+        const completeButtonSelector = type => $(`input.completeQuest[data-questtype=${type}]`);
         const jumpFwdButtonSelector = () => $('#roaJumpNextMob');
-        const beginQuestButtonsSelector = () => $('input.questRequest');
-        const questButtons = {
-            Battle: 0,
-            Harvesting: 1,
-            Profession: 2,
-        };
+        const beginQuestButtonsSelector = type => $(`input.questRequest[data-questtype=${type}]`);
+        const closeModalSelector = () => $('#modalWrapper > div > span.closeModal');
 
-        console.info('Setting up auto quest completion');
-        return setInterval(async () => {
-            await safeClick(infoLinkSelector(), postClickDelayMillis);
-            await safeClick(completeButtonSelector(), postClickDelayMillis);
-            for (let i = 0; i < settings.questCompletion.jumpForwardTimes; i++) {
-                await safeClick(jumpFwdButtonSelector(), postClickDelayMillis);
+        log('Setting up auto quest completion');
+        return setInterval(() => {
+            log('Checking quest...');
+            if (infoLinkSelector()) {
+                log('Found completed quest');
+
+                const questType = getCurrentQuestType();
+                log(`Current quest: ${questType}`);
+
+                safeClick(completeButtonSelector(questType));
+                if (questType === 'battle') {
+                    for (let i = 0; i < settings.questCompletion.jumpForwardTimes; i++) {
+                        safeClick(jumpFwdButtonSelector());
+                    }
+                }
+                safeClick(beginQuestButtonsSelector(questType));
+                safeClick(closeModalSelector()); // TODO make sure it close at first try
+                log('Refreshed quest');
+            } else {
+                log('No completed quest found');
+                safeClick(closeModalSelector());
             }
-            await safeClick(beginQuestButtonsSelector()[questButtons.Battle], postClickDelayMillis);
-        }, 60 * 1000);
+        }, settings.questCompletion.checkIntervalSeconds * 1000);
     };
 
-    if (settings.refresh.enabled) timers.autoRefresh = setupAutoRefresh();
-    if (settings.questCompletion.enabled) timers.questCompletion = setupQuestCompletion();
+    const start = () => {
+        log('Starting RoA Bot');
+        if (settings.refresh.enabled) timers.autoRefresh = setupAutoRefresh();
+        if (settings.questCompletion.enabled) timers.questCompletion = setupQuestCompletion();
+
+        printTimers();
+    };
+
+    start();
 })();
