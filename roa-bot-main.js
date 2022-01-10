685bd435d7f2ae8323f103814c76bbea4ae6320a
mdrozdovz <somebody.put.something@gmail.com>
2022-01-10 23:28:34 +0000

    - Bumped version

--- roa-bot-main.js
+++ roa-bot-main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            RoA-Bot
 // @namespace       http://tampermonkey.net/
-// @version         0.1.1
+// @version         0.1.2
 // @description     try to take over the world!
 // @author          mdrozdovz
 // @match           https://*.avabur.com/game*
